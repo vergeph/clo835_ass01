b22348c5f3b21cb44a149265f2a39184ac4b4bae
EC2 Default User <ec2-user@ip-172-31-3-22.ec2.internal>
2022-09-24 19:55:56 +0000

    update install_update.sh

--- terraform_code/dev/instances/install_update.sh
+++ terraform_code/dev/instances/install_update.sh
@@ -4,3 +4,4 @@
     sudo yum install git -y
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
+    docker network create -d bridge --subnet 185.18.0.1/24 --gateway 185.18.0.1 ass01net
