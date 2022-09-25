dc2a7fc6193960d4af9bedc8ebaa930765deb508
EC2 Default User <ec2-user@ip-172-31-3-22.ec2.internal>
2022-09-25 20:54:16 +0000

    test alb

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -118,3 +118,37 @@ resource "aws_ecr_repository" "ecr_repository" {
 data "aws_iam_instance_profile" "lab_profile" {
   name = "LabInstanceProfile"
 }
+
+# Test ALB 
+data "aws_subnet_ids" "public_sn" {
+vpc_id = data.aws_vpc.default.id
+}
+
+#subnets = data.aws_subnet_ids.public_sn.ids
+
+resource "aws_lb_listener" "alb" {
+  load_balancer_arn = aws_lb.alb.arn
+  port     = "80"
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+    type             = "forward"
+  }
+}
+
+ resource "aws_lb_target_group" "alb_tg" {
+  name     = "${local.name_prefix}-${var.env}-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+ }
+
+ resource "aws_lb" "alb" {
+  name               = "${local.name_prefix}-${var.env}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.my_sg.id]
+  subnets = data.aws_subnet_ids.public_sn.ids
+  enable_deletion_protection = false
+}
